0c9542af10e1f466cffa27166d31f2fccc8caa72
imilay11 <imilay11@gmail.com>
2023-07-18 22:10:16 +0300

    .

--- server/src/users/usersController.js
+++ server/src/users/usersController.js
@@ -72,7 +72,7 @@ const logout = async (req, res) => {
     const data = await UserService.logout(req.cookies);
 
     res.clearCookie('refreshToken', {
-      domain: '.vercel.app', // Replace with your actual domain folks-food.vercel.app
+      domain: 'folks-food.vercel.app', // Replace with your actual domain folks-food.vercel.app
       path: '/',
       secure: true,
       httpOnly: true,
